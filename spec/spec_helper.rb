184fbc29cf3bf221f1b5fb44286b9ee8c32f13f8
Jackson Pires <jackson.pires@gmail.com>
2014-03-27 15:22:59 -0300

    Adicionado Test Coverage do CodeClimate.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
+# Require Code Climate Test Reporter
+require "codeclimate-test-reporter"
+
+# Starts Code Climate Test Reporter
+CodeClimate::TestReporter.start
+
+# Require other files of project
 require "cnpj_utils"
 require_relative "../lib/cnpj_utils/cnpj"
