a232a4cebee091f1e7f0deceb5dc46ae7e125383
Jackson Pires <jackson.pires@gmail.com>
2014-04-03 09:05:46 -0300

    Adicionado o Coveralls. Melhorado o README.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,12 @@ require "codeclimate-test-reporter"
 # Starts Code Climate Test Reporter
 CodeClimate::TestReporter.start
 
+# Require Coveralls for Test cover
+require 'coveralls'
+
+# Starts Coderalls
+Coveralls.wear!
+
 # Require other files of project
 require "cnpj_utils"
 require_relative "../lib/cnpj_utils/cnpj"
